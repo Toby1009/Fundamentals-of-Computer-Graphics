387a7bb06c513696f9117cc9e922ebd261b4cf21
Toby1009 <st99005912@gmail.com>
2024-03-03 17:33:13 +0800

    add hw

---
+++ hw/cg-gui-opengl1x/src/Scene/MainScene.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+
+#include "MainScene.h"
+
+
+
+namespace CG
+{
+    MainScene::MainScene()
+    {
+        glColor3f(1, 0, 0);
+    }
+
+    MainScene::~MainScene()
+    {
+    }
+
+    auto MainScene::Initialize() -> bool
+    {
+        glEnable(GL_DEPTH_TEST);
+        glCullFace(GL_BACK);
+        glEnable(GL_CULL_FACE);
+        
+        return true;
+    }
+
+    void MainScene::Update(double dt)
+    {
+
+    }
+
+    void MainScene::Render()
+    {
+        glClearColor(0.0, 0.0, 0.0, 1); // Black background
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glPolygonMode(GL_FRONT_AND_BACK, mode);
+
+        //init 
+        bigAngle = PI / 2 + rotated; // init angle 90
+        smallAngle = PI / 2 + ANGLE_Diff / 2 + rotated; //init angle 90
+        r = 0.167;//small cycle
+        R = 0.5; //big cycle
+        originPos = { 0,0 };
+
+        for (int i = 0; i < 10; i++)
+        {
+
+            bigOPos = { originPos.x + R * cos(bigAngle),originPos.y + R * sin(bigAngle) };
+            smallOPos = { originPos.x + r * cos(smallAngle) , originPos.y + r * sin(smallAngle) };
+
+            glBegin(GL_POLYGON);
+            if (i % 2 == 0)
+            {
+                glVertex2f(bigOPos.x, bigOPos.y);
+                glVertex2f(smallOPos.x, smallOPos.y);
+                glVertex2f(originPos.x, originPos.y);
+            }
+            else
+            {
+                glVertex2f(originPos.x, originPos.y);
+                glVertex2f(smallOPos.x, smallOPos.y);
+                glVertex2f(bigOPos.x, bigOPos.y);
+            }
+
+            glEnd();
+            if (i % 2 == 0)
+            {
+                bigAngle += ANGLE_Diff;
+            }
+            else
+            {
+                smallAngle += ANGLE_Diff;
+            }
+        }
+    }
+
+    void MainScene::OnResize(int width, int height)
+    {
+        std::cout << "MainScene Resize: " << width << " " << height << std::endl;
+    }
+
+    void MainScene::OnKeyboard(int key, int action)
+    {
+        std::cout << "MainScene OnKeyboard: " << key << " " << action << std::endl;
+
+        if (action == GLFW_RELEASE)
+        {
+            switch (key)
+            {
+            case GLFW_KEY_1: // Red
+                glColor3f(1, 0, 0);
+                break;
+            case GLFW_KEY_2 : // green
+                glColor3f(0, 1, 0);
+                break;
+            case GLFW_KEY_3: // blue
+                glColor3f(0, 0, 1);
+                break;
+            case GLFW_KEY_4:
+                rotated += PI / 4;
+                break;
+            case GLFW_KEY_5:
+                rotated -= PI / 4;
+                break;
+            }
+        }
+    }
+
+    void MainScene::SetMode(int mode)
+    {
+        switch (mode)
+        {
+        case 0:
+            this->mode = GL_FILL;
+            break;
+        case 1:
+            this->mode = GL_LINE;
+            break;
+        default:
+            this->mode = GL_FILL;
+            break;
+        }
+    }
+}
---
+++ hw/cg-gui-opengl1x/src/Scene/MainScene.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <array>
+#include <string>
+#include <map>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#define PI 3.1415926f
+
+namespace CG
+{
+	class MainScene
+	{
+	public:
+
+		struct Pos {
+			GLfloat x, y; 
+		};
+		MainScene();
+		~MainScene();
+
+		auto Initialize() -> bool;
+		void Update(double dt);
+		void Render();
+
+		void OnResize(int width, int height);
+		void OnKeyboard(int key, int action);
+
+		void SetMode(int mode);
+
+	private:
+		GLenum mode = GL_FILL; // GL_FILL or GL_LINE
+		const GLfloat ANGLE_Diff = 2*PI / 5; // cycle dovoded into five equal parts
+		GLfloat bigAngle ; // init angle 90
+		GLfloat smallAngle; //init angle 90
+		GLfloat r;//small cycle
+		GLfloat R; //big cycle
+		Pos originPos;
+		Pos smallOPos, bigOPos; //small cycle pos , big cycle pos
+		GLfloat rotated = 0;
+	};
+}
+
